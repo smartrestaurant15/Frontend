53aebf9f64e1b34d20ebd2d348a959281054db67
Parca15 <142187202+Parca15@users.noreply.github.com>
2026-03-25 22:22:28 -0500

    UI/UX: Modernizacion a Glassmorphism, accesibilidad corregida y escalado de fuentes en auth y dashboard

--- src/app/shared/components/ui/accessibility/accessibility.component.ts
+++ src/app/shared/components/ui/accessibility/accessibility.component.ts
@@ -11,7 +11,7 @@ export class AccessibilityComponent implements OnInit {
     invertColors = false;
     underlineLinks = false;
     readableFont = false;
-    fontSize = 14;
+    fontSize = 16;
 
     constructor() { }
 
