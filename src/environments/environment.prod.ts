b3ba149cee773dfe82e3b6810e5c063b015f41c8
samuelasta <samuela.castanom@uqvirtual.edu.co>
2026-04-19 14:53:38 -0500

    enviroment apiUrl to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://smartrestaurant-tbbb.onrender.com/api',
+  apiUrl: 'https://api.smart-restaurant.online/api',
   apiTimeout: 30000,
   googleClientId: '512756127640-hm9hitju6rp7d749v8v9tiifqj19b3ko.apps.googleusercontent.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://api.smart-restaurant.online/api',
   apiTimeout: 30000,
   googleClientId: '512756127640-hm9hitju6rp7d749v8v9tiifqj19b3ko.apps.googleusercontent.com'
 };
