b2689449d24669d85094c93cb501ae6d8a630de9
Johanc1970 <johanc.garciag@uqvirtual.edu.co>
2026-04-19 22:37:36 -0500

    feat: add chatbot API key header to all requests

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.smart-restaurant.online/api',
   apiTimeout: 30000,
-  googleClientId: '512756127640-hm9hitju6rp7d749v8v9tiifqj19b3ko.apps.googleusercontent.com'
+  googleClientId: '512756127640-hm9hitju6rp7d749v8v9tiifqj19b3ko.apps.googleusercontent.com',
+  chatbotApiKey: 'CHATBOT_API_KEY_PLACEHOLDER'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api.smart-restaurant.online/api',
+  apiUrl: 'http://localhost:8080/api',
   apiTimeout: 30000,
-  googleClientId: '512756127640-hm9hitju6rp7d749v8v9tiifqj19b3ko.apps.googleusercontent.com'
+  googleClientId: '512756127640-hm9hitju6rp7d749v8v9tiifqj19b3ko.apps.googleusercontent.com',
+  chatbotApiKey: 'CHATBOT_API_KEY_PLACEHOLDER'
 };
